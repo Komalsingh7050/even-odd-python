1744da03cc3109229c6059ee690bda2c392884b4
Komalsingh7050 <komalsinghrajput7050@gmail.com>
2026-01-21 12:35:27 +0530

    Add code to guess number program
    Implement number guessing game with input handling.

--- guess-the-number.py
+++ guess-the-number.py
@@ -1,1 +1,14 @@
+import random
 
+number = random.randint(1, 100)
+while True:
+   try:
+    guess = int(input("guess this number between 1 to 100 "))
+    if guess < number:
+      print("too low")
+    elif guess > number:
+      print("too high")
+    else:
+      print("Well Done")
+   except ValueError:
+    print("Enter a valid number ")
